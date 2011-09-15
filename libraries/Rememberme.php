b111b293b073308fe1f50a834f470a717d90709a
Joe Auty <joe@netmusician.org>
2011-09-15 17:05:15 -0500

    recordOrigPage and getOrigPage functions

--- libraries/Rememberme.php
+++ libraries/Rememberme.php
@@ -18,42 +18,65 @@ class Rememberme {
 		$this->CI =& get_instance();
 	}
 	
-	function setCookie($netid = "") {
-		if (!$netid) {
+	function setCookie($netid = "", $nocookie = false) {
+		if (!$netid && !$nocookie) {
 			show_error("setCookie request missing netid");
 			return;
 		}
 		
+		session_start();
 		$this->CI->load->library('user_agent');	
 		
-		// delete any cookies currently owned by netid
-		$this->CI->db->where('netid', $netid);
-		$this->CI->db->delete('ci_cookies');
-			
-		$cookie_id = uniqid('', true);			
+		// delete any existing table entries belonging to user
+		$nocookie ? $this->CI->db->where('php_session_id', session_id()) : 
+					$this->CI->db->where('netid', $netid);		
+		$this->CI->db->delete('ci_cookies');	
+		
+		$nocookie ? $cookie_id = "" : $cookie_id = uniqid('', true);
+	
 		$insertdata = array(
 			'cookie_id' => $cookie_id,
 			'ip_address' => $_SERVER['REMOTE_ADDR'],
 			'user_agent' => $this->CI->agent->agent_string(),
 			'netid' => $netid,
-			'created_at' => date('Y-m-d H:i:s')
-		);
-
-		$this->CI->db->insert('ci_cookies', $insertdata);						
+			'created_at' => date('Y-m-d H:i:s'),
+			'orig_page_requested' => $this->CI->uri->uri_string(),
+			'php_session_id' => session_id()
+		);	
+		$this->CI->db->insert('ci_cookies', $insertdata);
 		
-		// set cookie for 1 year
-		$cookie = array(
-			'name' => 'rememberme_token',
-			'value' => $cookie_id,
-			'expire' => 31557600,
-			'domain' => '.' . $_SERVER['SERVER_NAME'],
-			'path' => preg_replace('/^(http|https):\/\/' . $_SERVER['SERVER_NAME'] . '/', '', base_url()),
-			'secure' => isset($_SERVER['HTTPS']) ? $_SERVER['HTTPS'] : 0
-		);
-		set_cookie($cookie);
+		if (!$nocookie) {
+			// set cookie for 1 year
+			$cookie = array(
+				'name' => 'rememberme_token',
+				'value' => $cookie_id,
+				'expire' => 31557600,
+				'domain' => '.' . $_SERVER['SERVER_NAME'],
+				'path' => $apppath,
+				'secure' => isset($_SERVER['HTTPS']) ? $_SERVER['HTTPS'] : 0
+			);
+			set_cookie($cookie);
 		
-		// establish session
-		$this->CI->session->set_userdata('rememberme_session', $netid);
+			// establish session
+			$this->CI->session->set_userdata('rememberme_session', $netid);	
+		}		
+	}
+	
+	function recordOrigPage() {
+		$this->setCookie("", true);
+	}
+	
+	function getOrigPage() {
+		session_start();
+		$query = $this->CI->db->get_where('ci_cookies', array(
+			'php_session_id' => session_id()
+		));
+		if ($query->num_rows()) {
+			return $query->row()->orig_page_requested;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	function deleteCookie() {
@@ -73,10 +96,13 @@ class Rememberme {
 		delete_cookie('rememberme_token');
 	}
 	
-	function verifyCookie() {											
+	function verifyCookie() {			
+		if (!get_cookie('rememberme_token')) { return false; }
+									
 		$query = $this->CI->db->get_where('ci_cookies', array(
 			'cookie_id' => get_cookie('rememberme_token')			
 		));
+
 		if ($query->num_rows()) {
 			$row = $query->row();
 			
